73d7d36747829db784985fb2c356cda4d197ea76
Matias <126021442+Matias-87@users.noreply.github.com>
2025-03-07 20:56:37 -0300

    final version of certant clients section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientsSectionComponent } from "./clients-section/clients-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ClientsSectionComponent]
 })
 export class AppComponent {
   title = 'certant-web-fix';
---
+++ src/app/clients-section/clients-section.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './clients-section.component.html',
+  styleUrl: './clients-section.component.scss'
+})
+export class ClientsSectionComponent implements OnInit {
+  clientLogos: string[][] = [
+    ['./imgs/clientes/BCBA.png', 'logo BCBA'],
+    ['./imgs/clientes/banco-provincia.png', 'logo banco provincia'],
+    ['./imgs/clientes/santander.png', 'logo santander'],
+    ['./imgs/clientes/Logo_Oficial_Banco_Comafi.png', 'logo comafi'],
+    ['./imgs/clientes/itau.png', 'logo itau'],
+    ['./imgs/clientes/iplan.jpg', 'logo iplan'],
+    ['./imgs/clientes/logo_turismo.gif', 'logo turismo'],
+    ['./imgs/clientes/nextel.png', 'logo nextel'],
+  ] 
+
+  ngOnInit(): void {
+    for (let i = 0; i < 2; i++) {
+      this.clientLogos.forEach(logo => {
+        this.clientLogos.push(logo);
+      });
+    }
+  }
+}
